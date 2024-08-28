5dbab9e18523e3bf67d31bc5fb279130281c895f
Kanishk Debnath <134499777+kanishkdebnath@users.noreply.github.com>
2024-08-28 15:29:23 +0530

    Add application.yml

--- src/main/java/com/kanishk/demo/controller/HelloController.java
+++ src/main/java/com/kanishk/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.kanishk.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
-        return "Hello! This is a simple Spring application.";
+        return "Hello! This is a simple Spring application." + welcomeMessage;
     }
 
     @GetMapping(value = "/hey")
